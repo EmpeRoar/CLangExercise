0b15902f3e2345b95027d124a890ccf6346fbf37
Julius Bacosa <juliusbacosa@gmail.com>
2021-09-16 08:10:04 +0800

    ...

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one1 = 1;
+    int two2 = 2;
+    int sum = one1 + two2;
+
+    for(int i = 0; i<=10; i++) {
+        cout << i << "\n";
+    }
+
+
+    cout << "Hello World " << sum << endl;
+
+}
